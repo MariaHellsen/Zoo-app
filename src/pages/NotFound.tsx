32a5f2d40c1fada43fd21455283da65c82348c51
MariaHellsen <maria.hellsen@outlook.com>
2025-06-18 15:01:09 +0200

    Added tailwind classes to NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,21 @@
+import { NavLink } from "react-router-dom";
 export const NotFound = () => {
   return (
-    <>
-      <h2>Oops!Fel stig! Denna sida leder ingenstans.</h2>
-      <h3>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h2 className="m-8 p-7 text-4xl font-bold text-center ">
+        Oops! Fel stig! Denna sida leder ingenstans.
+      </h2>
+      <h3 className="p-7 text-center">
         Ta en ny väg! För att komma igång med matningen igen, navigera till
-        sidan "Djuren" och välj enkelt ett djur genom att klicka på dess bild.
+        sidan "
+        <NavLink
+          to={"/animals"}
+          className="font-bold text-blue-600 hover:underline"
+        >
+          Djuren
+        </NavLink>
+        " och välj enkelt ett djur genom att klicka på dess bild.
       </h3>
-    </>
+    </div>
   );
 };
